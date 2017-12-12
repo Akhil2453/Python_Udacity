a392d606259f00a2c07c494ec301dbbc2f76183d
Akhil <akhil.menon94@gmail.com>
2017-12-12 14:27:20 +0530

    added profanity check

--- profanitycheck.py
+++ profanitycheck.py
@@ -1,7 +1,18 @@
+import urllib
+
+
 def rd_txt():
 	quotes = open("/home/akhil/Desktop/Udacity/Python/profanity.txt")
 	cof = quotes.read()
 	print(cof)
 	quotes.close()
+	chk_profanity(cof)
+
+def chk_profanity(text_to_check):
+	conn = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+	output = conn.read()
+	print(output)
+	conn.close()
+
 
 rd_txt()
