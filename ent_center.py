6c5a0b16e58d3b8fd9ab691560672f6747ae9ca6
Akhil <akhil.menon94@gmail.com>
2017-12-12 15:12:54 +0530

    added methoda to instance

--- ent_center.py
+++ ent_center.py
@@ -1,3 +1,6 @@
 import media
 
-toy_story = media.Movie()
+toy_story = media.Movie("Toy Story",
+						"A story of a boy and his toys that come to life",
+						"https://www.google.co.in/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiv49Sil4TYAhUKYo8KHc_9DwsQjRwIBw&url=https%3A%2F%2Fwww.ebay.com%2Fitm%2FTOY-STORY-MOVIE-POSTER-1-Sided-ORIGINAL-CHASE-27x40-DISNEY-TIM-ALLEN-TOM-HANKS-%2F151720821950&psig=AOvVaw2Otev4oDxYvLorzKvaOEiR&ust=1513158082017850"
+						"https://www.google.co.in/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwibi-2vl4TYAhVIqo8KHYr4AX0QyCkIKDAA&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DKYz2wyBy3kc&usg=AOvVaw0z39p4CAtfAw4uZDhaE4vX")
