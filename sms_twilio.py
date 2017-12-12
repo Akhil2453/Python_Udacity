48ca9e7de64cfeb4f677f92d2d4548932fe13d7f
Akhil <akhil.menon94@gmail.com>
2017-12-12 13:48:34 +0530

    tried removing error

--- sms_twilio.py
+++ sms_twilio.py
@@ -1,15 +1,20 @@
 #from twilio.rest import TwilioRestClient
-from twilio import rest
+#from twilio import rest
 
 # Your Account sid and Auth token from twilio.com/user/account
 
-account_sid = "ACc165f64757b95fa2ba936fdd4cc01df0"
-auth_token = "e7393d8c9319cb9468eb8e38454b7c1b"
+#account_sid = "ACc165f64757b95fa2ba936fdd4cc01df0"
+#auth_token = "e7393d8c9319cb9468eb8e38454b7c1b"
 
-client = rest.TwilioRestClient(account_sid, auth_token)
+#client = rest.TwilioRestClient(account_sid, auth_token)
 
-message = client.sms.messages.create(
-	body = "Nishita?! I Love you <3",
-	to = "+918655752503S",
-	from_="+16153921256 ")
-print message.sid
+#message = client.sms.messages.create(
+#	body = "Nishita?! I Love you <3",
+#	to = "+918655752503S",
+#	from_="+16153921256 ")
+#print message.sid
+
+
+import twilio
+
+client = twilio.rest.TwilioRestClient()
---
+++ turtle_miniproj.py
@@ -0,0 +1,29 @@
+import turtle
+
+def square(akhil):
+	for i in range(4):
+		akhil.forward(100)
+		akhil.right(90)
+
+def art():
+	win = turtle.Screen()
+	win.bgcolor("red")
+
+	judy = turtle.Turtle()
+	judy.shape("turtle")
+	judy.color("green")
+	judy.speed(2)
+
+	for i in range(36):
+		square(judy)
+		judy.right(10)
+
+	#martha = turtle.Turtle()
+	#martha.shape("arrow")
+	#martha.color("blue")
+	#martha.circle(100)
+
+	win.exitonclick()
+
+
+art()
