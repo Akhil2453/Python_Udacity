84b8417bb7169abbcbe742e1e51b2c1dfb1fe94f
Akhil <akhil.menon94@gmail.com>
2017-12-12 13:38:17 +0530

    made small changes

--- sms_twilio.py
+++ sms_twilio.py
@@ -1,11 +1,12 @@
-from twilio.rest import TwilioRestClient
+#from twilio.rest import TwilioRestClient
+from twilio import rest
 
 # Your Account sid and Auth token from twilio.com/user/account
 
 account_sid = "ACc165f64757b95fa2ba936fdd4cc01df0"
 auth_token = "e7393d8c9319cb9468eb8e38454b7c1b"
 
-client = TwilioRestClient(account_sid, auth_token)
+client = rest.TwilioRestClient(account_sid, auth_token)
 
 message = client.sms.messages.create(
 	body = "Nishita?! I Love you <3",
