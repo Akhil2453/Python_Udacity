736e0b91edcde98c66bf77635d8a015954fbc14c
Akhil <akhil.menon94@gmail.com>
2017-12-12 13:00:28 +0530

    Code to send a sms

---
+++ sms_twilio.py
@@ -0,0 +1,14 @@
+from twilio.rest import TwilioRestClient
+
+# Your Account sid and Auth token from twilio.com/user/account
+
+account_sid = "ACc165f64757b95fa2ba936fdd4cc01df0"
+auth_token = "e7393d8c9319cb9468eb8e38454b7c1b"
+
+client = TwilioRestClient(account_sid, auth_token)
+
+message = client.sms.messages.create(
+	body = "Nishita?! I Love you <3",
+	to = "+918655752503S",
+	from_="+16153921256 ")
+print message.sid
