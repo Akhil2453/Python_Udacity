36c446185a9ee75f9a01258a3e002039d102feab
Akhil <akhil.menon94@gmail.com>
2017-12-12 15:21:42 +0530

    added another instance avatar

--- ent_center.py
+++ ent_center.py
@@ -2,5 +2,13 @@ import media
 
 toy_story = media.Movie("Toy Story",
 						"A story of a boy and his toys that come to life",
-						"https://www.google.co.in/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiv49Sil4TYAhUKYo8KHc_9DwsQjRwIBw&url=https%3A%2F%2Fwww.ebay.com%2Fitm%2FTOY-STORY-MOVIE-POSTER-1-Sided-ORIGINAL-CHASE-27x40-DISNEY-TIM-ALLEN-TOM-HANKS-%2F151720821950&psig=AOvVaw2Otev4oDxYvLorzKvaOEiR&ust=1513158082017850"
+						"https://www.google.co.in/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiv49Sil4TYAhUKYo8KHc_9DwsQjRwIBw&url=https%3A%2F%2Fwww.ebay.com%2Fitm%2FTOY-STORY-MOVIE-POSTER-1-Sided-ORIGINAL-CHASE-27x40-DISNEY-TIM-ALLEN-TOM-HANKS-%2F151720821950&psig=AOvVaw2Otev4oDxYvLorzKvaOEiR&ust=1513158082017850",
 						"https://www.google.co.in/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwibi-2vl4TYAhVIqo8KHYr4AX0QyCkIKDAA&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DKYz2wyBy3kc&usg=AOvVaw0z39p4CAtfAw4uZDhaE4vX")
+
+print(toy_story.storyline)
+
+avatar = media.Movie("Avatar",
+					 "A marine on an alien planet",
+					 "https://www.google.co.in/imgres?imgurl=http%3A%2F%2Fwww.joblo.com%2Fposters%2Fimages%2Ffull%2Favatar-quad-1.jpg&imgrefurl=http%3A%2F%2Fwww.joblo.com%2Fmovie-posters%2F2009%2Favatar&docid=3MwXGD67OJ7M8M&tbnid=EoAiNDYAUGtX3M%3A&vet=10ahUKEwi8s7GTmYTYAhVHtY8KHXLtBM8QMwjVASgCMAI..i&w=1100&h=825&bih=633&biw=651&q=avatar%20poster&ved=0ahUKEwi8s7GTmYTYAhVHtY8KHXLtBM8QMwjVASgCMAI&iact=mrc&uact=8",
+					 "https://www.google.co.in/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwjc-OqnmYTYAhUM6Y8KHaYmBiwQyCkIKDAA&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DcRdxXPV9GNQ&usg=AOvVaw28UmdagwZ7OOiwbgOOa-_f")
+print(avatar.storyline)
