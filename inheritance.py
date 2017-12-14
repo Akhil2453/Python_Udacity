5e7eca23a0af3bc886ccc95b9a833a4e5b87b327
Akhil <akhil.menon94@gmail.com>
2017-12-14 10:12:08 +0530

    created child class to demonstrate inheritance

--- inheritance.py
+++ inheritance.py
@@ -3,6 +3,19 @@ class Parent():
 		print "Parent constructor called"
 		self.last_name = last_name
 		self.eye_color = eye_color
-billy_cyrus = Parent("Cyrus", "blue")
 
-print(billy_cyrus.last_name)
+class Child(Parent):
+	def __init__(self, last_name, eye_color, noT):
+		print("child constructor called")
+		Parent.__init__(self, last_name, eye_color)
+		self.noT = noT
+
+
+
+#billy_cyrus = Parent("Cyrus", "blue")
+
+#print(billy_cyrus.last_name)
+
+miley_cyrus = Child("Cyrus", "blue", "three")
+print(miley_cyrus.last_name)
+print(miley_cyrus.noT)
