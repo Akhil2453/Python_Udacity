6b561918e509f2bf101dc24f19c8a47611bbd725
Akhil <akhil.menon94@gmail.com>
2017-12-14 10:55:11 +0530

    demonstrated method overriding

--- inheritance.py
+++ inheritance.py
@@ -4,11 +4,21 @@ class Parent():
 		self.last_name = last_name
 		self.eye_color = eye_color
 
+	def show_info(self):
+		print("Last name : "+self.last_name)
+		print("Eye Color : "+self.eye_color)
+
 class Child(Parent):
 	def __init__(self, last_name, eye_color, noT):
 		print("child constructor called")
 		Parent.__init__(self, last_name, eye_color)
 		self.noT = noT
+
+	def show_info(self):
+		print("Last name : "+self.last_name)
+		print("Eye Color : "+self.eye_color)
+		print("Number of Toys :"+self.noT)
+
 
 
 
